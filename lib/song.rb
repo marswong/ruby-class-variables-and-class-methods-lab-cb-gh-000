132da6d67441c646da0fd51a65b3272af2ced546
Mars Wong <marswong618@gmail.com>
2018-01-30 03:51:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,6 +23,22 @@ class Song
   end
 
   def genres
-    @@genre
+    @@genres
+  end
+
+  def genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      genre_hash[genre] = @@genres.count(genre)
+    end
+    genre_hash
+  end
+
+  def artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      artist_hash[artist] = @@genres.count(artist)
+    end
+    artist_hash
   end
 end
